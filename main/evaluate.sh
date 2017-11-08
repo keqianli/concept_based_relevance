6208ba2427ddb63a908290ca722fbfceb2506ca1
keqianli <keqianlimms@gmail.com>
2017-11-08 14:16:44 -0800

    helper

--- main/evaluate.sh
+++ main/evaluate.sh
@@ -1,10 +1,11 @@
-LABELS_FILE=../input/arxiv_cs_small_labels_num.txt
-FINAL_RELEVANCE=$OUTPUT_DIR/relevance.txt
-
+LABELS_FILE=${LABELS_FILE:-../input/arxiv_cs_small_labels_num.txt}
+FINAL_RELEVANCE=${FINAL_RELEVANCE:''}
 
 source conf.d/autoPhrase.conf
-
 export OUTPUT_DIR=../output/$MODEL
+FINAL_RELEVANCE=${FINAL_RELEVANCE:$OUTPUT_DIR/relevance.txt}
+
+
 # SEGGED_TEXT_TFIDF=$OUTPUT_DIR/tfidf
 # FINAL_RELEVANCE=$SEGGED_TEXT_TFIDF.relevance
 
